3077b132188a315ebfd9bb3e750cb9cc1ceff378
Soresu <g_mester@freemail.hu>
2016-01-26 22:44:39 +0100

    Jax

--- UnderratedAIO/UnderratedAIO/Helpers/IncomingDamage.cs
+++ UnderratedAIO/UnderratedAIO/Helpers/IncomingDamage.cs
@@ -31,9 +31,11 @@ namespace UnderratedAIO.Helpers
             foreach (var d in data)
             {
                 Console.WriteLine(d.Hero.Name);
-                Console.WriteLine("\t DamageCount" + d.DamageCount);
-                Console.WriteLine("\t DamageTaken" + d.DamageTaken);
-                Console.WriteLine("\t TargetedCC" + d.TargetedCC);
+                Console.WriteLine("\t DamageCount: " + d.DamageCount);
+                Console.WriteLine("\t DamageCount: " + d.AADamageCount);
+                Console.WriteLine("\t DamageTaken: " + d.DamageTaken);
+                Console.WriteLine("\t DamageTaken: " + d.AADamageTaken);
+                Console.WriteLine("\t TargetedCC: " + d.TargetedCC);
             }
         }
 
@@ -54,7 +56,14 @@ namespace UnderratedAIO.Helpers
 
         private void Game_OnGameUpdate(EventArgs args)
         {
-            resetData();
+            if (ObjectManager.Player.IsDead)
+            {
+                resetAllData();
+            }
+            else
+            {
+                resetData();
+            }
         }
 
         private void resetData()
@@ -74,7 +83,16 @@ namespace UnderratedAIO.Helpers
                         }
                     }
                 }
-                incDamage.TargetedCC = false;
+            }
+        }
+
+        private void resetAllData()
+        {
+            foreach (var incDamage in
+                IncomingDamagesAlly.Concat(IncomingDamagesEnemy))
+            {
+                incDamage.Damages.Clear();
+                incDamage.DamageCount = 0;
             }
         }
 
@@ -97,7 +115,7 @@ namespace UnderratedAIO.Helpers
                             if (Orbwalking.IsAutoAttack(args.SData.Name))
                             {
                                 var dmg = (float) sender.GetAutoAttackDamage(target, true);
-                                data.Damages.Add(new Dmg(dmg, missileSpeed));
+                                data.Damages.Add(new Dmg(dmg, missileSpeed, true));
                                 data.DamageCount++;
                             }
                             else
@@ -108,14 +126,9 @@ namespace UnderratedAIO.Helpers
                                     data.Damages.Add(
                                         new Dmg(
                                             (float) Damage.GetSpellDamage(hero, (Obj_AI_Base) args.Target, args.Slot),
-                                            missileSpeed));
+                                            missileSpeed, CombatHelper.isTargetedCC(args.SData.Name, true)));
                                     data.DamageCount++;
                                 }
-                            }
-                            if (sender is Obj_AI_Hero && target != null && target.IsAlly && !target.IsMe &&
-                                CombatHelper.isTargetedCC(args.SData.Name, true) && args.SData.Name != "NasusW")
-                            {
-                                data.TargetedCC = true;
                             }
                         }
                     }
@@ -130,12 +143,31 @@ namespace UnderratedAIO.Helpers
         public List<Dmg> Damages = new List<Dmg>();
         public int DamageCount;
         public Obj_AI_Hero Hero;
-        public bool TargetedCC;
+
 
         public float DamageTaken
         {
             get { return Damages.Sum(d => d.DamageTaken); }
-            set { DamageTaken = value; }
+        }
+
+        public float HealthPrediction
+        {
+            get { return Hero.Health - DamageTaken; }
+        }
+
+        public bool TargetedCC
+        {
+            get { return Damages.Any(d => d.TargetedCC); }
+        }
+
+        public float AADamageTaken
+        {
+            get { return Damages.Where(d => d.isAA).Sum(d => d.DamageTaken); }
+        }
+
+        public float AADamageCount
+        {
+            get { return Damages.Count(d => d.isAA); }
         }
 
         public IncData(Obj_AI_Hero _hero)
@@ -149,12 +181,16 @@ namespace UnderratedAIO.Helpers
         public float DamageTaken;
         public float Time;
         public float delete;
+        public bool isAA;
+        public bool TargetedCC;
 
-        public Dmg(float dmg, float delete)
+        public Dmg(float dmg, float delete, bool isAA = false, bool TargetedCC = false)
         {
             DamageTaken = dmg;
             Time = Game.Time;
             this.delete = delete;
+            this.isAA = isAA;
+            this.TargetedCC = TargetedCC;
         }
     }
 }
