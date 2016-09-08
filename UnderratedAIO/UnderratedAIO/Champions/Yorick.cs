8b9a92babe1692d665ce54384ce2df992bf7ec07
Soresu <g_mester@freemail.hu>
2016-09-08 21:13:44 +0200

    6.18

--- UnderratedAIO/UnderratedAIO/Champions/Yorick.cs
+++ UnderratedAIO/UnderratedAIO/Champions/Yorick.cs
@@ -56,20 +56,37 @@ namespace UnderratedAIO.Champions
                 default:
                     break;
             }
-            if (Yorickghost && R.IsReady())
+            if (config.Item("autoWStun", true).GetValue<bool>() && W.IsReady())
             {
-                PetHandler.MovePet(config, orbwalker.ActiveMode);
+                var targ =
+                    HeroManager.Enemies.Where(
+                        hero =>
+                            W.CanCast(hero) &&
+                            (hero.HasBuffOfType(BuffType.Snare) || hero.HasBuffOfType(BuffType.Stun) ||
+                             hero.HasBuffOfType(BuffType.Taunt) || hero.HasBuffOfType(BuffType.Suppression)))
+                        .OrderByDescending(hero => TargetSelector.GetPriority(hero))
+                        .ThenBy(hero => hero.Health)
+                        .FirstOrDefault();
+                if (targ != null)
+                {
+                    W.CastOnUnit(targ);
+                }
             }
         }
 
         private static void AfterAttack(AttackableUnit unit, AttackableUnit target)
         {
+            if (CanQ2)
+            {
+                return;
+            }
             var nearestMob = Jungle.GetNearest(player.Position);
             if (unit.IsMe && Q.IsReady() &&
-                ((orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && config.Item("useq").GetValue<bool>() &&
-                  target is Obj_AI_Hero) ||
-                 (config.Item("useqLC").GetValue<bool>() && nearestMob != null &&
-                  nearestMob.Distance(player.Position) < player.AttackRange + 30)))
+                (((orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && config.Item("useq", true).GetValue<bool>()) ||
+                  (orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed &&
+                   config.Item("useqH", true).GetValue<bool>())) && target is Obj_AI_Hero) ||
+                (config.Item("useqLC", true).GetValue<bool>() && nearestMob != null &&
+                 nearestMob.Distance(player.Position) < player.AttackRange + 30))
             {
                 Q.Cast();
                 Orbwalking.ResetAutoAttackTimer();
@@ -79,8 +96,13 @@ namespace UnderratedAIO.Champions
 
         private void beforeAttack(Orbwalking.BeforeAttackEventArgs args)
         {
+            if (CanQ2)
+            {
+                return;
+            }
             if (args.Unit.IsMe && Q.IsReady() && orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear &&
-                config.Item("useqLC").GetValue<bool>() && !(args.Target is Obj_AI_Hero) && (args.Target.Health > 700))
+                config.Item("useqLC", true).GetValue<bool>() && !(args.Target is Obj_AI_Hero) &&
+                (args.Target.Health > 700))
             {
                 Q.Cast();
                 player.IssueOrder(GameObjectOrder.AutoAttack, args.Target);
@@ -100,39 +122,35 @@ namespace UnderratedAIO.Champions
                 ItemHandler.UseItems(target, config, combodmg);
             }
             bool hasIgnite = player.Spellbook.CanUseSpell(player.GetSpellSlot("SummonerDot")) == SpellState.Ready;
-            if (config.Item("usew").GetValue<bool>() && W.CanCast(target))
+            var wPred = W.GetPrediction(target);
+            if (config.Item("usew", true).GetValue<bool>() && W.CanCast(target) && wPred.Hitchance >= HitChance.High)
             {
-                W.Cast(target.Position);
+                W.Cast(wPred.CastPosition);
             }
-            if (config.Item("usee").GetValue<bool>() && E.CanCast(target))
+            if (config.Item("usee", true).GetValue<bool>() && E.IsReady())
             {
-                E.CastOnUnit(target);
+                CastE(target);
             }
-            if (!Yorickghost && config.Item("user").GetValue<bool>() && R.IsReady())
+            if (config.Item("user", true).GetValue<bool>() && R.IsReady() && R.CanCast(target) &&
+                target.HealthPercent < config.Item("userHp", true).GetValue<Slider>().Value &&
+                ((!Orbwalking.CanAttack() && Orbwalking.CanMove(100)) ||
+                 !config.Item("useronlyMelee", true).GetValue<bool>()))
             {
-                var ally =
-                    HeroManager.Allies.Where(
-                        i =>
-                            !i.IsDead && player.Distance(i) < R.Range &&
-                            ((i.Health * 100 / i.MaxHealth) <= config.Item("atpercenty").GetValue<Slider>().Value ||
-                             Program.IncDamages.GetAllyData(i.NetworkId).IsAboutToDie) &&
-                            !config.Item("ulty" + i.SkinName).GetValue<bool>() && i.CountEnemiesInRange(750) > 0)
-                        .OrderByDescending(i => Environment.Hero.GetAdOverTime(player, i, 5))
-                        .FirstOrDefault();
-                if (ally != null && R.IsInRange(ally))
-                {
-                    R.Cast(ally);
-                }
+                R.CastOnUnit(target);
             }
             if (config.Item("useIgnite").GetValue<bool>() && combodmg > target.Health && hasIgnite)
             {
                 player.Spellbook.CastSpell(player.GetSpellSlot("SummonerDot"), target);
             }
+            if (Q.IsReady() && config.Item("useq2", true).GetValue<bool>() && CanQ2)
+            {
+                Q.Cast();
+            }
         }
 
-        private static bool Yorickghost
+        private static bool CanQ2
         {
-            get { return player.Spellbook.GetSpell(SpellSlot.R).Name == "YorickReviveAllyGuide"; }
+            get { return player.Spellbook.GetSpell(SpellSlot.Q).Name == "YorickQ2"; }
         }
 
         private void Harass()
@@ -142,42 +160,47 @@ namespace UnderratedAIO.Champions
             {
                 return;
             }
-            if (config.Item("usewH").GetValue<bool>() && W.CanCast(target))
+
+            if (config.Item("useeH", true).GetValue<bool>())
             {
-                W.Cast(target);
+                CastE(target);
             }
-            if (config.Item("useeH").GetValue<bool>() && E.CanCast(target))
+            if (Q.IsReady() && config.Item("useq2H", true).GetValue<bool>() && CanQ2)
             {
-                E.CastOnUnit(target);
+                Q.Cast();
+            }
+        }
+
+        private void CastE(Obj_AI_Hero target)
+        {
+            var pred = E.GetPrediction(target);
+            var poly = GetPoly(pred.UnitPosition);
+            if (pred.Hitchance >= HitChance.High && pred.CastPosition.Distance(player.Position) < 700)
+            {
+                E.Cast(pred.CastPosition);
             }
         }
 
         private void Clear()
         {
-            float perc = (float) config.Item("minmana").GetValue<Slider>().Value / 100f;
+            float perc = (float) config.Item("minmana", true).GetValue<Slider>().Value / 100f;
             if (player.Mana < player.MaxMana * perc)
             {
                 return;
             }
-            var bestpos =
-                W.GetCircularFarmLocation(
-                    MinionManager.GetMinions(W.Range, MinionTypes.All, MinionTeam.NotAlly, MinionOrderTypes.MaxHealth),
-                    100);
-            if (config.Item("usewLC").GetValue<bool>() && W.IsReady() &&
-                config.Item("usewLChit").GetValue<Slider>().Value <= bestpos.MinionsHit)
+
+            if (config.Item("useeLC", true).GetValue<bool>() && E.IsReady())
             {
-                W.Cast(bestpos.Position);
+                var minis = MinionManager.GetMinions(E.Range, MinionTypes.All, MinionTeam.NotAlly);
+                var ePos = E.GetCircularFarmLocation(minis, 300);
+                var poly = GetPoly(ePos.Position.To3D());
+                Console.WriteLine(minis.Count(m => poly.IsInside(m.Position)));
+                if (minis.Count(m => poly.IsInside(m.Position)) >= config.Item("useeMin", true).GetValue<Slider>().Value)
+                {
+                    E.Cast(ePos.Position);
+                }
             }
-            var target =
-                MinionManager.GetMinions(E.Range, MinionTypes.All, MinionTeam.NotAlly)
-                    .Where(i => i.Health < E.GetDamage(i) || i.Health > 600f)
-                    .OrderByDescending(i => i.Distance(player))
-                    .FirstOrDefault();
-            if (config.Item("useeLC").GetValue<bool>() && E.CanCast(target))
-            {
-                E.CastOnUnit(target);
-            }
-            if (config.Item("useqLC").GetValue<bool>() && Q.IsReady())
+            if (config.Item("useqLC", true).GetValue<bool>() && Q.IsReady() && !CanQ2)
             {
                 var targetQ =
                     MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.NotAlly)
@@ -198,10 +221,9 @@ namespace UnderratedAIO.Champions
 
         private void Game_OnDraw(EventArgs args)
         {
-            DrawHelper.DrawCircle(config.Item("drawaa").GetValue<Circle>(), player.AttackRange);
-            DrawHelper.DrawCircle(config.Item("drawww").GetValue<Circle>(), W.Range);
-            DrawHelper.DrawCircle(config.Item("drawee").GetValue<Circle>(), E.Range);
-            DrawHelper.DrawCircle(config.Item("drawrr").GetValue<Circle>(), R.Range);
+            DrawHelper.DrawCircle(config.Item("drawww", true).GetValue<Circle>(), W.Range);
+            DrawHelper.DrawCircle(config.Item("drawee", true).GetValue<Circle>(), 700);
+            DrawHelper.DrawCircle(config.Item("drawrr", true).GetValue<Circle>(), R.Range);
             HpBarDamageIndicator.Enabled = config.Item("drawcombo").GetValue<bool>();
         }
 
@@ -239,15 +261,41 @@ namespace UnderratedAIO.Champions
             return (float) damage;
         }
 
+        private Geometry.Polygon GetPoly(Vector3 castPos)
+        {
+            var eWidth = 200;
+            var startPos = player.ServerPosition.Extend(castPos, player.Distance(castPos) - 100);
+            var endPos = player.ServerPosition.Extend(castPos, player.Distance(castPos) + 400);
+            var direction = (endPos.To2D() - startPos.To2D()).Normalized();
+
+            var pos1 = (startPos.To2D() - direction.Perpendicular() * eWidth / 2f).To3D();
+
+            var pos2 =
+                (endPos.To2D() + (endPos.To2D() - startPos.To2D()).Normalized() +
+                 direction.Perpendicular() * eWidth / 2f).To3D();
+
+            var pos3 = (startPos.To2D() + direction.Perpendicular() * eWidth / 2f).To3D();
+
+            var pos4 =
+                (endPos.To2D() + (endPos.To2D() - startPos.To2D()).Normalized() -
+                 direction.Perpendicular() * eWidth / 2f).To3D();
+            var poly = new Geometry.Polygon();
+            poly.Add(pos1);
+            poly.Add(pos3);
+            poly.Add(pos2);
+            poly.Add(pos4);
+            return poly;
+        }
+
         private void InitYorick()
         {
             Q = new Spell(SpellSlot.Q, player.AttackRange);
             W = new Spell(SpellSlot.W, 600);
-            W.SetSkillshot(
-                W.Instance.SData.SpellCastTime, W.Instance.SData.LineWidth, W.Speed, false,
-                SkillshotType.SkillshotCircle);
-            E = new Spell(SpellSlot.E, 550);
-            R = new Spell(SpellSlot.R, 850);
+            W.SetSkillshot(0.75f, W.Instance.SData.LineWidth, 1200, false, SkillshotType.SkillshotCircle);
+            E = new Spell(SpellSlot.E, 1000);
+            E.SetSkillshot(0.5f, 200, 1200, false, SkillshotType.SkillshotCircle);
+            R = new Spell(SpellSlot.R, 600);
+            R.SetSkillshot(0.5f, 200, 1800, false, SkillshotType.SkillshotCircle);
         }
 
         private void InitMenu()
@@ -265,50 +313,44 @@ namespace UnderratedAIO.Champions
 
             // Draw settings
             Menu menuD = new Menu("Drawings ", "dsettings");
-            menuD.AddItem(new MenuItem("drawaa", "Draw AA range"))
+            menuD.AddItem(new MenuItem("drawww", "Draw W range", true))
                 .SetValue(new Circle(false, Color.FromArgb(180, 116, 99, 45)));
-            menuD.AddItem(new MenuItem("drawww", "Draw W range"))
+            menuD.AddItem(new MenuItem("drawee", "Draw E range", true))
                 .SetValue(new Circle(false, Color.FromArgb(180, 116, 99, 45)));
-            menuD.AddItem(new MenuItem("drawee", "Draw E range"))
-                .SetValue(new Circle(false, Color.FromArgb(180, 116, 99, 45)));
-            menuD.AddItem(new MenuItem("drawrr", "Draw R range"))
+            menuD.AddItem(new MenuItem("drawrr", "Draw R range", true))
                 .SetValue(new Circle(false, Color.FromArgb(180, 116, 99, 45)));
             menuD.AddItem(new MenuItem("drawcombo", "Draw combo damage")).SetValue(true);
             config.AddSubMenu(menuD);
             // Combo Settings
             Menu menuC = new Menu("Combo ", "csettings");
-            menuC.AddItem(new MenuItem("useq", "Use Q")).SetValue(true);
-            menuC.AddItem(new MenuItem("usew", "Use W")).SetValue(true);
-            menuC.AddItem(new MenuItem("usee", "Use E")).SetValue(true);
-            menuC.AddItem(new MenuItem("user", "Use R")).SetValue(true);
-            menuC.AddItem(new MenuItem("atpercenty", "Ult friend under")).SetValue(new Slider(30, 0, 100));
+            menuC.AddItem(new MenuItem("useq", "Use Q", true)).SetValue(true);
+            menuC.AddItem(new MenuItem("useq2", "Use Q2", true)).SetValue(true);
+            menuC.AddItem(new MenuItem("usew", "Use W", true)).SetValue(true);
+            menuC.AddItem(new MenuItem("usee", "Use E", true)).SetValue(true);
+            menuC.AddItem(new MenuItem("user", "Use R", true)).SetValue(true);
+            menuC.AddItem(new MenuItem("userHp", "Under enemy health%", true)).SetValue(new Slider(50, 1, 100));
+            menuC.AddItem(new MenuItem("useronlyMelee", "Only in melee range", true)).SetValue(true);
             menuC.AddItem(new MenuItem("useIgnite", "Use Ignite")).SetValue(true);
             menuC = ItemHandler.addItemOptons(menuC);
             config.AddSubMenu(menuC);
             // Harass Settings
             Menu menuH = new Menu("Harass ", "Hsettings");
-            menuH.AddItem(new MenuItem("usewH", "Use W")).SetValue(true);
-            menuH.AddItem(new MenuItem("useeH", "Use E")).SetValue(true);
+            menuH.AddItem(new MenuItem("useqH", "Use Q", true)).SetValue(true);
+            menuH.AddItem(new MenuItem("useq2H", "Use Q2", true)).SetValue(true);
+            menuH.AddItem(new MenuItem("useeH", "Use E", true)).SetValue(false);
             config.AddSubMenu(menuH);
             // LaneClear Settings
             Menu menuLC = new Menu("LaneClear ", "Lcsettings");
-            menuLC.AddItem(new MenuItem("useqLC", "Use Q")).SetValue(true);
-            menuLC.AddItem(new MenuItem("usewLC", "Use W")).SetValue(true);
-            menuLC.AddItem(new MenuItem("usewLChit", "Min hit")).SetValue(new Slider(3, 1, 8));
-            menuLC.AddItem(new MenuItem("useeLC", "Use E")).SetValue(true);
-            menuLC.AddItem(new MenuItem("minmana", "Keep X% mana")).SetValue(new Slider(1, 1, 100));
+            menuLC.AddItem(new MenuItem("useqLC", "Use Q", true)).SetValue(true);
+            menuLC.AddItem(new MenuItem("useeLC", "Use E", true)).SetValue(true);
+            menuLC.AddItem(new MenuItem("useeMin", "Min hit", true)).SetValue(new Slider(3, 1, 10));
+            menuLC.AddItem(new MenuItem("minmana", "Keep X% mana", true)).SetValue(new Slider(1, 1, 100));
             config.AddSubMenu(menuLC);
             // Misc Settings
             Menu menuM = new Menu("Misc ", "Msettings");
             menuM = DrawHelper.AddMisc(menuM);
+            menuM.AddItem(new MenuItem("autoWStun", "Auto W on stun", true)).SetValue(true);
             config.AddSubMenu(menuM);
-            var sulti = new Menu("Don't ult on ", "dontult");
-            foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsAlly))
-            {
-                sulti.AddItem(new MenuItem("ulty" + hero.SkinName, hero.SkinName)).SetValue(false);
-            }
-            config.AddSubMenu(sulti);
-            
             config.AddItem(new MenuItem("UnderratedAIO", "by Soresu v" + Program.version.ToString().Replace(",", ".")));
             config.AddToMainMenu();
         }
